a87bb07ccc7d12db07c5a0b1e75212d4eb507bc5
noamhecht <noam.hecht@gmail.com>
2020-04-01 13:52:09 +0300

    birthday field needs to be changed

--- app/forms.py
+++ app/forms.py
@@ -1,31 +1,42 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
 from .models import Musician
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 
 class SignUpForm(UserCreationForm):
 
     email = forms.EmailField()
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(max_length=20)
+    birth = forms.DateField(widget=DateInput)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name', 'birth', 'email', 'password1', 'password2', )
 
 
 class UserUpdateForm(forms.ModelForm):
 
     email = forms.EmailField()
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(max_length=20)
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'first_name', 'last_name', 'email']
 
 
 class MusicianUpdateForm(forms.ModelForm):
     class Meta:
         model = Musician
-        fields = ['instrument', 'playing_level', 'image']
+        birthday = forms.DateField(widget=DateInput)
+        fields = ['birthday', 'instrument', 'playing_level', 'image']
 
 
 class SearchForm(forms.ModelForm):
